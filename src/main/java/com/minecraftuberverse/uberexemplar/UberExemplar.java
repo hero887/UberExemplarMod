809e954a25805319a350662feb011e74a17bf1ef
hero887 <h3r087@gmail.com>
2016-08-28 17:12:17 +0800

    Initial commit
    Set up reference, mod_id, & inits

---
+++ src/main/java/com/minecraftuberverse/uberexemplar/UberExemplar.java
@@ -0,0 +1,27 @@
+package com.minecraftuberverse.uberexemplar;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class UberExemplar {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
